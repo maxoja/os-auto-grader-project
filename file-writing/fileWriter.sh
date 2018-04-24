4cf836df98cdfd8005b6d67e2e9064e225478697
17chuchu <17chuchu.guy@gmail.com>
2018-04-24 18:46:46 +0700

    Finish File Writing Section
    - 2 function are given

---
+++ file-writing/fileWriter.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+writeTestResult ()
+{
+	filename="result$1.txt" 
+	echo "$2;$3" >> $filename
+}
+
+clearResultFile()
+{
+	filename="result$1.txt"
+	> $filename
+}
+
+clearResultFile Lab11
+writeTestResult Lab11 59090002 20
