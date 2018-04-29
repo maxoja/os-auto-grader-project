1e2b5841f223c4628b44f69372a2a73f03ad2548
panupongv <most54321@gmail.com>
2018-04-29 17:47:19 +0700

    fixed Bad substitution

--- Home/to_fix.sh
+++ Home/to_fix.sh
@@ -1,59 +1,75 @@
 #!/bin/sh
 
-lab_name=$1                 #question name (LabXY) = 1st parameter
-lab_num=${1:0:4}             #slice LabXY to LabX (first layer directory name)
-answer=$2                 #answer = 2nd parameter
-cd Labs                 #goto Labs folder
+#question name (LabXY) = 1st parameter
+lab_name=$1
 
-if [ -e $lab_num ]             #if folder LabX exist
+#slice LabXY to LabX (first layer directory name)			
+lab_num=`echo $lab_name | cut -b 1-4`	
+
+#answer = 2nd parameter	
+answer=$2   
+
+#goto Labs folder              
+cd Labs                 
+
+#if folder LabX exist
+if [ -e $lab_num ]             
 then
-    cd $lab_num             #goto that folder
-
+	#goto that folder
+	cd $lab_num             
 else
-    echo "$lab_num does not exist"    #print error message
-    exit 1                #terminates program
+	#print error message
+	echo "$lab_num does not exist"    
+	#terminates program
+	exit 1                
 fi
 
-set `ls`                #set positional parameters($1, $2, ..., $n) to name of files and folders in current directory
-                    #in this case, folder of students.
+#set positional parameters($1, $2, ..., $n) to name of files and folders in current directory
+#in this case, folder of students.
+set `ls`                
 
-for item in $*                #for item in those file/folder names
+#for item in those file/folder names
+for item in $*                
 do
-    if [ -d $item ]            #if the name is directory
-    then
-        cd $item        #go in
-        if gcc $lab_name.c -o temp;
-        then
-              #compile success
-              echo "compile ok"
+	#if the name is directory
+	if [ -d $item ]            
+    	then
+		#go in
+        	cd $item        
+        	if gcc $lab_name.c -o temp;
+        	then
+              		#compile success
+              		echo "compile ok"
 
-              #generate temp files
+              		#generate temp files
               
-              ./temp > student_result.txt
-              echo $answer > teacher_result.txt
+              		./temp > student_result.txt
+              		echo $answer > teacher_result.txt
               
-              #evaluate result
-              if [ "$(cat student_result.txt)" == "$(cat teacher_result.txt)" ];
-              then
-                    #answer correct
-                    echo "correct answer"
-                    score = 3
-              else
-                    #answer wrong
-                    echo "wrong answer"
-                    score = 2
-              fi
+              		#evaluate result
+              		if [ "$(cat student_result.txt)" == "$(cat teacher_result.txt)" ];
+              		then
+                    		#answer correct
+                    		echo "correct answer"
+                    		score=3
+              		else
+                    		#answer wrong
+                    		echo "wrong answer"
+                    		score=2
+              		fi
 
-         #clear temp files
-          else
-             #compile error
-             echo "compile failed"
-             score = 1
-          fi
+		#clear temp files
+		else
+             		#compile error
+             		echo "compile failed"
+             		score=1
+         	fi
         else
-            score = 0
-        fi
-        cd ..            #go out
-        echo "$item;$score" >> "result$lab_name.txt"
-    fi
+        	score=0
+       	fi
+	#go out
+        cd ..          
+	#file result to file  
+       	echo "$item;$score" >> "result$lab_name.txt"
+	
 done
